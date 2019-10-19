04121e13551c93040478bc9933a11ce1db8c8b3a
Hack Chyson <chyson@aliyun.com>
2019-10-19 16:57:48 +0800

    spider

--- sms/sms.py
+++ sms/sms.py
@@ -1,15 +1,5 @@
 from twilio.rest import Client
 
-# Your Account SID from twilio.com/console
-account_sid = "AC27de07df1f83e3975b0914b8c3057fe5"
-# Your Auth Token from twilio.com/console
-auth_token = '7002bf873d8744bf5cd919d07e4acd04'
-client = Client(account_sid, auth_token)
-message = client.messages.create(
-    to="+8615101551318",
-    from_="+17029797636",
-    body="exercise")
-
 
 class Sender:
     def __init__(self,
---
+++ spider/weather.py
@@ -0,0 +1,22 @@
+import requests
+from lxml import etree
+
+
+def get_msg(url='http://www.tianqi.com/tongzhou/life.html'):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
+    response = requests.get(url, headers=headers)
+
+    html = response.text
+
+    html_xpath = etree.HTML(html)
+
+    rain = html_xpath.xpath('/html/body/div[4]/div/ul/li/p/text()')[0]
+    ultraviolet_ray = html_xpath.xpath('/html/body/div[4]/div/ul/li[3]/p/text()')[0].strip('。')
+    clothes = html_xpath.xpath('/html/body/div[4]/div/ul/li[6]/p/text()')[0].strip('。')
+    air = html_xpath.xpath('/html/body/div[4]/div/div[3]/dl/dd/b/text()')[0]
+    temperature = html_xpath.xpath('/html/body/div[4]/div/div[3]/dl/dd/text()')[0]
+    air_quality = html_xpath.xpath('/html/body/div[4]/div/div[3]/dl/dd[2]/b/text()')[0]
+
+    msg = '；\n'.join([rain, ultraviolet_ray, clothes, air, temperature, air_quality, ''])
+    return msg
