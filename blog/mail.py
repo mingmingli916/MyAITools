967aa52e2b53f069bb2487910a9ee6377e514428
Hack Chyson <chyson@aliyun.com>
2019-10-22 10:42:30 +0800

    decorators;

--- blog/mail.py
+++ blog/mail.py
@@ -1,6 +1,7 @@
 import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
+from chyson.decorators import decorators
 import decorators
 
 decorators.basicConfig(level=decorators.INFO,
--- decorators/decorators.py
+++ decorators/decorators.py
@@ -54,37 +54,45 @@ def bounded(minimum, maximum):
     return decorator
 
 
-if __debug__:
-    logger = logging.getLogger('Logger')
-    logger.setLevel(logging.DEBUG)
-    handler = logging.FileHandler(os.path.join(tempfile.gettempdir(), 'logged.log'))
+def logged(file=None):
+    if __debug__:
+        logger = logging.getLogger('Logger')
+        logger.setLevel(logging.DEBUG)
+        if file is not None:
+            handler = logging.FileHandler(file)
+        else:
+            handler = logging.FileHandler(os.path.join(tempfile.gettempdir(), 'logged.log'))
 
-    logger.addHandler(handler)
+        logger.addHandler(handler)
 
+        def decorator(function):
+            @functools.wraps(function)
+            def wrapper(*args, **kwargs):
+                log = 'called: ' + function.__name__ + '('
+                log += ', '.join(
+                    ['{0!r}'.format(a) for a in args] + ['{0!s}={1!r}'.format(k, v) for k, v in kwargs.items()])
+                result = exception = None
+                try:
+                    result = function(*args, *kwargs)
+                    return result
+                except Exception as err:
+                    exception = err
+                finally:
+                    log += (
+                        (') -> ' + str(result)) if exception is None else ') {0}: {1}'.format(type(exception),
+                                                                                              exception))
+                    logger.debug(log)
+                    if exception is not None:
+                        raise exception
 
-    def logged(function):
-        @functools.wraps(function)
-        def wrapper(*args, **kwargs):
-            log = 'called: ' + function.__name__ + '('
-            log += ', '.join(
-                ['{0!r}'.format(a) for a in args] + ['{0!s}={1!r}'.format(k, v) for k, v in kwargs.items()])
-            result = exception = None
-            try:
-                result = function(*args, *kwargs)
-                return result
-            except Exception as err:
-                exception = err
-            finally:
-                log += (
-                    (') -> ' + str(result)) if exception is None else ') {0}: {1}'.format(type(exception), exception))
-                logger.debug(log)
-                if exception is not None:
-                    raise exception
+            return wrapper
 
-        return wrapper
-else:
-    def logged(function):
-        return function
+        return decorator
+    else:
+        def decorator(function):
+            return function
+
+        return decorator
 
 
 # If we want to give meaning to annotations, for example, to provide type checking,
