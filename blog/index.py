0bba6715bbcc5f090f389771f7d69060a81c85c8
Mike Chyson <chyson@aliyun.com>
2019-11-27 23:16:38 +0800

    ai

--- blog/index.py
+++ blog/index.py
@@ -142,7 +142,7 @@ def generate_show():
         if ref in black_list:
             continue
         line += '<div>'
-        line += '<img src="/show/{}" width="600px">'.format(ref)
+        line += '<a href="http://chyson.net/show/{}">><img src="/show/{}" width="600px"></a>'.format(ref, ref)
         line += '</div>\n'
     line += '</div>\n'
     return line
