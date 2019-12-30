8e945f86b8045c9246450505f692fcb6ee71a2c0
Mike Chyson <chyson@aliyun.com>
2019-12-30 14:42:37 +0800

    convert modelnet40 off file into graph;

---
+++ parse/model_net_40_to_graph.py
@@ -0,0 +1,137 @@
+import numpy as np
+
+PREFIX = 'OFF'
+off_sep = ' '
+encoding = 'utf8'
+vertex_start = 2
+grap_sep = ' '
+
+"""
+off file format
+
+OFF numVertices numFaces numEdges
+x y z
+x y z
+... numVertices like above
+NVertices v1 v2 v3 ... vN
+MVertices v1 v2 v3 ... vM
+... numFaces like above
+
+OFF
+8 6 0
+-0.500000 -0.500000 0.500000
+0.500000 -0.500000 0.500000
+-0.500000 0.500000 0.500000
+0.500000 0.500000 0.500000
+-0.500000 0.500000 -0.500000
+0.500000 0.500000 -0.500000
+-0.500000 -0.500000 -0.500000
+0.500000 -0.500000 -0.500000
+4 0 1 3 2
+4 2 3 5 4
+4 4 5 7 6
+4 6 7 1 0
+4 1 7 5 3
+4 6 0 2 4
+"""
+
+
+def model_net_40_to_graph(input_file,
+                          output_file,
+                          off_sep=off_sep,
+                          graph_sep=grap_sep,
+                          encoding=encoding,
+                          vertex_start=vertex_start):
+    """
+    Convert a ModelNet40 OFF file into a list stored graph.
+
+    :param input_file: input off file
+    :param output_file: output graph file
+    :param off_sep: off separator
+    :param graph_sep: graph separator
+    :param encoding:
+    :param vertex_start: starting vertex from which line
+    :return:
+    """
+    fh = open(input_file, 'r', encoding=encoding)
+    lines = fh.read().rstrip().split('\n')
+
+    # vertices faces edges
+    vfe = lines[1].rstrip()
+
+    num_vertices = int(vfe.split(off_sep)[0])
+    vertices = lines[vertex_start:vertex_start + num_vertices]
+    faces = lines[vertex_start + num_vertices:]
+
+    pos_dict = index_position(vertices, off_sep)
+    graph = faces2graph(pos_dict, faces, graph_sep)
+
+    save_graph(output_file, graph, graph_sep)
+
+
+def index_position(lst, sep):
+    """
+    index a vertex list, starting from 0
+    :param lst: vertex list
+    :param sep: separator
+    :return:
+    """
+    pos_dict = dict()
+    for no, p in enumerate(lst):
+        value = p.split(sep)
+        value = [float(v) for v in value]
+        pos_dict[no] = value
+    return pos_dict
+
+
+def faces2graph(pos_dict, faces, graph_sep):
+    """
+    Convert indices list to graph.
+    :param pos_dict: position directory
+    :param faces: 3D object faces
+    :param graph_sep: graph separator
+    :return:
+    """
+    graph = {}
+    for face in faces:
+        lst = face.split(off_sep)
+        num_edges = int(lst[0])
+        edges = lst[1:]
+
+        for e in range(num_edges):
+            idx1 = int(edges[e])
+            idx2 = int(edges[(e + 1) % num_edges])
+
+            # remove the duplicated lines like (1,0) and (1,0) are the same line.
+            if idx1 > idx2:
+                tmp = idx1
+                idx1 = idx2
+                idx2 = tmp
+
+            graph['{}{}{}'.format(idx1, grap_sep, idx2)] = distance(np.array(pos_dict[idx1]), np.array(pos_dict[idx2]))
+
+    return graph
+
+
+def distance(point1, point2):
+    """
+    Compute the distance of two points.
+    :param point1:
+    :param point2:
+    :return:
+    """
+    return np.linalg.norm(point1 - point2)
+
+
+def save_graph(filename, graph, sep):
+    """
+    Save graph into a file.
+    :param filename:
+    :param graph:
+    :param sep:
+    :return:
+    """
+    with open(filename, 'w') as fh:
+        for i in sorted(graph.keys(), key=lambda x: int(x.split(sep)[0])):
+            fh.write(sep.join([i, str(graph[i])]))
+            fh.write('\n')
