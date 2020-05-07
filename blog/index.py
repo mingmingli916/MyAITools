0bfbcdec63ed31b152c079c77768703e830f7804
Mike Chyson <chyson@aliyun.com>
2020-05-07 20:21:32 +0800

    regexp

--- blog/index.py
+++ blog/index.py
@@ -4,7 +4,7 @@ import re
 path = '/var/www/html'
 website = 'https://chyson.net'
 
-black_list = [r'index.html', r'.git', r'.aes', r'.gitignore', r'\W*.md', r'\W*.org', r'private']
+black_list = [r'index.html', r'.git', r'.aes', r'.gitignore', r'\W*.md', r'\W*.org', r'private$']
 img_list = ['pics', 'pic', 'pictures', 'picture', 'fun']
 video_suffix = ['.mp4', '.ogg', '.webm']
 show_list = ['csapp',
